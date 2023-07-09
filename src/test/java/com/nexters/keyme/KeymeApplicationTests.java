bc28c66f42938549744ddf761455cc9e09db25c4
Senglee Choi <tmdfl1027@gmail.com>
2023-07-09 22:13:16 +0900

    프로젝트 초기 설정

---
+++ src/test/java/com/nexters/keyme/KeymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexters.keyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
