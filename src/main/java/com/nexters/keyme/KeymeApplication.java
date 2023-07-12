4a2e4aaa874eb3832d4aa593d24dbde7ab772ff0
Senglee Choi <tmdfl1027@gmail.com>
2023-07-13 01:18:43 +0900

    ✨ 어플리케이션 @EnableAsync 애노테이션 추가
    NotificationRequestHandler에서 사용된 @Async가 활성화되도록 함

--- src/main/java/com/nexters/keyme/KeymeApplication.java
+++ src/main/java/com/nexters/keyme/KeymeApplication.java
@@ -2,8 +2,10 @@ package com.nexters.keyme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class KeymeApplication {
 
 	public static void main(String[] args) {
