517c40af32603bc8ca8d070950b9b617cb331960
Senglee Choi <tmdfl1027@gmail.com>
2023-07-13 01:19:52 +0900

    ✨ NotificationRequest 빌더 정의
    info를 추가할 때 가독성 향상을 위해 빌더 정의

--- src/main/java/com/nexters/keyme/notification/dto/NotificationRequest.java
+++ src/main/java/com/nexters/keyme/notification/dto/NotificationRequest.java
@@ -1,5 +1,6 @@
 package com.nexters.keyme.notification.dto;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -12,6 +13,37 @@ public class NotificationRequest {
         this.title = title;
         this.body = body;
         this.info = info;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String title;
+        private String body;
+        private Map<String, String> info = new HashMap<>();
+
+        private Builder() {}
+
+        public Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder body(String body) {
+            this.body = body;
+            return this;
+        }
+
+        public Builder info(String key, String value) {
+            this.info.put(key, value);
+            return this;
+        }
+
+        public NotificationRequest build() {
+            return new NotificationRequest(this.title, this.body, this.info);
+        }
     }
 
     public String getTitle() {
