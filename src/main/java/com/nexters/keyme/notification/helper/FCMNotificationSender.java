83876c6c5fb2a2005bce56a30f6048ab5cad153d
Senglee Choi <tmdfl1027@gmail.com>
2023-07-15 07:36:58 +0900

    ✨ FCMNotificationSender 정의
    - NotificationService 내에서 의존하는 FCMNotificationSender 정의

---
+++ src/main/java/com/nexters/keyme/notification/helper/FCMNotificationSender.java
@@ -0,0 +1,59 @@
+package com.nexters.keyme.notification.helper;
+
+import com.google.firebase.messaging.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Component
+public class FCMNotificationSender {
+
+    private final FirebaseMessaging firebaseMessaging;
+
+
+    public void sendByTokens(List<String> tokens, String title, String body) {
+        Notification notification = Notification.builder()
+                .setTitle(title)
+                .setBody(body)
+                .build();
+
+        List<Message> messages = tokens.stream()
+                .map(token ->
+                        Message.builder()
+                                .setNotification(notification)
+                                .setToken(token)
+                                .build())
+                .collect(Collectors.toList());
+
+
+        try {
+            firebaseMessaging.sendAll(messages);
+        } catch (FirebaseMessagingException e) {
+            throw new RuntimeException("메시지 발송 중 문제가 발생했습니다.");
+        }
+    }
+
+    public void sendByTopics(List<String> topics, String title, String body) {
+        Notification notification = Notification.builder()
+                .setTitle(title)
+                .setBody(body)
+                .build();
+
+        List<Message> messages = topics.stream()
+                .map(topic ->
+                        Message.builder()
+                                .setNotification(notification)
+                                .setTopic(topic)
+                                .build())
+                .collect(Collectors.toList());
+
+        try {
+            firebaseMessaging.sendAll(messages);
+        } catch (FirebaseMessagingException e) {
+            throw new RuntimeException("메시지 발송 중 문제가 발생했습니다.");
+        }
+    }
+}
