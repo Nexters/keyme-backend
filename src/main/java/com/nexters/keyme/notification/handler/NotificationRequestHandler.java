e4f3e70fcb66b9a47b1c4669a96f5ff137cd7ec7
Senglee Choi <tmdfl1027@gmail.com>
2023-07-15 12:21:08 +0900

    ✨ 알림 비동기 처리 시 Thread Pool 설정 클래스 작성
    최대 스레드 사이즈 등 설정. 세부 값은 추후 서버 환경에 맞추어 변경 필요.

--- src/main/java/com/nexters/keyme/notification/handler/NotificationRequestHandler.java
+++ src/main/java/com/nexters/keyme/notification/handler/NotificationRequestHandler.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
-@Async
+@Async("NotificationThreadPool")
 @Component
 public class NotificationRequestHandler {
 
