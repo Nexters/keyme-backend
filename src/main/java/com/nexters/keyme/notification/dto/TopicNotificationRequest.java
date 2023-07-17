90c7a36d77636280e28bbc4b6c578368ff081856
Senglee Choi <tmdfl1027@gmail.com>
2023-07-17 17:49:16 +0900

    🎨 NotificationRequest 빌더 Singular 애노테이션 사용하도록 변경
    - NotificationRequest에서 Map 필드의 빌더에서 (key, value) 형태로 값을 추가할 수 있도록 Singular 애노테이션을 추가함
    - 직접 구현한 기존 빌더를 롬복 Builder 애노테이션으로 대체

--- src/main/java/com/nexters/keyme/notification/dto/TopicNotificationRequest.java
+++ src/main/java/com/nexters/keyme/notification/dto/TopicNotificationRequest.java
@@ -1,18 +1,24 @@
 package com.nexters.keyme.notification.dto;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+@Builder
 @RequiredArgsConstructor
 public class TopicNotificationRequest {
     @Getter
-    private List<Long> topicIds;
+    private final List<Long> topicIds;
     @Getter
     private final String title;
     @Getter
     private final String body;
+    @Singular("info")
     private final Map<String, String> info;
+
+    public Optional<String> getInfo(String key) {
+        return Optional.of(info.get(key));
+    }
 }
--- src/main/java/com/nexters/keyme/notification/dto/UserNotificationRequest.java
+++ src/main/java/com/nexters/keyme/notification/dto/UserNotificationRequest.java
@@ -1,57 +1,22 @@
 package com.nexters.keyme.notification.dto;
 
-import lombok.Getter;
+import lombok.*;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@Builder
+@RequiredArgsConstructor
 public class UserNotificationRequest {
     @Getter
-    private List<Long> userIds;
+    private final List<Long> userIds;
     @Getter
     private final String title;
     @Getter
     private final String body;
+    @Singular("info")
     private final Map<String, String> info;
-
-    public UserNotificationRequest(String title, String body, Map<String, String> info) {
-        this.title = title;
-        this.body = body;
-        this.info = info;
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
-    public static class Builder {
-        private String title;
-        private String body;
-        private Map<String, String> info = new HashMap<>();
-
-        private Builder() {}
-
-        public Builder title(String title) {
-            this.title = title;
-            return this;
-        }
-
-        public Builder body(String body) {
-            this.body = body;
-            return this;
-        }
-
-        public Builder info(String key, String value) {
-            this.info.put(key, value);
-            return this;
-        }
-
-        public UserNotificationRequest build() {
-            return new UserNotificationRequest(this.title, this.body, this.info);
-        }
-    }
 
     public Optional<String> getInfo(String key) {
         return Optional.of(info.get(key));
