0182e3edb0e21eafb045f5a13fab9de1eb8faba5
RokwonK <wonrok97@naver.com>
2023-07-18 19:17:51 +0900

    ✨ Feign Client 활성화
    ⚡️ 성능 개선
    🔥 코드 / 파일 삭제
    🐛 버그 수정
    🚑 긴급 수정
    ✨ 새 기능
    📝 문서 추가 / 수정
    💄 UI, 스타일 추가 / 수정
    🎉 프로젝트 시작
    ✅ 테스트 추가 / 수정
    🔒 보안 이슈 수정
    🔖 릴리즈 / 버전 태그
    💚 CI 빌드 수정
    📌 특정 버전 의존성 고정
    👷 CI 빌드 시스템 추가 / 수정
    📈 분석, 추가 코드 추가 / 수정
    ♻️ 코드 리팩토링
    ➕ 의존성 추가
    ➖ 의존성 제거
    🔧 구성 파일 추가 / 삭제
    🔨 개발 스크립트 추가 / 수정
    🌐 국제화 / 현지화
    💩 똥싼 코드
    ⏪ 변경 내용 되돌리기
    🔀 브랜치 합병
    📦 컴파일된 파일 추가 / 수정
    👽 외부 API 변화로 인한 수정
    🚚 리소스 이동, 이름 변경
    📄 라이센스 추가 / 수정
    💡 주석 추가 / 수정
    🍻 술 취해서 쓴 코드
    🗃 데이터베이스 관련 수정
    🔊 로그 추가 / 수정
    🙈 .gitignore 추가 / 수정

--- src/main/java/com/nexters/keyme/KeymeApplication.java
+++ src/main/java/com/nexters/keyme/KeymeApplication.java
@@ -2,12 +2,12 @@ package com.nexters.keyme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class KeymeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KeymeApplication.class, args);
 	}
-
 }
