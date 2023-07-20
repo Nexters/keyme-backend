65dba6ac502659ab54f5b9a65395daf76d03dbb0
Senglee Choi <tmdfl1027@gmail.com>
2023-07-20 21:57:13 +0900

    ✨ 컨트롤러별 swagger 설명 추가
    DummyController, MemberController 설명 추가

--- src/main/java/com/nexters/keyme/dummy/DummyController.java
+++ src/main/java/com/nexters/keyme/dummy/DummyController.java
@@ -1,5 +1,6 @@
 package com.nexters.keyme.dummy;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,13 @@ public class DummyController {
     private final DummyService dummyService;
 
     @GetMapping("/hello")
+    @ApiOperation(value = "테스트용 컨트롤러(인증 불필요)", notes = "인증처리 없이 접근할 수 있는 엔드포인트")
     public String helloKeyme() {
         return "Hello keyme!";
     }
 
     @GetMapping("/auth")
+    @ApiOperation(value = "테스트용 컨트롤러(인증 필요)", notes = "인증 처리가 완료되어야 접근 가능한 엔드포인트")
     public String helloAuthKeyme() {
         return "Hello Authenticated keyme!";
     }
--- src/main/java/com/nexters/keyme/member/controller/MemberController.java
+++ src/main/java/com/nexters/keyme/member/controller/MemberController.java
@@ -3,22 +3,21 @@ package com.nexters.keyme.member.controller;
 import com.nexters.keyme.member.dto.request.LoginRequestDto;
 import com.nexters.keyme.member.dto.response.MemberResponseDto;
 import com.nexters.keyme.member.service.MemberService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
+
 public class MemberController {
   private final MemberService memberService;
-  @PostMapping
+  @PostMapping("/login")
+  @ApiOperation(value = "oauth 로그인", notes = "SDK에서 받은 access token(카카오)/identity token(애플)을 통해 로그인한다.")
   public MemberResponseDto signIn(
           @RequestBody LoginRequestDto request) {
     return memberService.getOrCreateMember(request);
