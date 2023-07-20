365f8e369272d0dbd0800c66a517b92dc7f1d9c7
Senglee Choi <tmdfl1027@gmail.com>
2023-07-20 21:31:15 +0900

    ✨ LoginRequestDto 프로퍼티 swagger 설명 추가
    각 필드별로 swagger에 프로퍼티 설명 추가

--- src/main/java/com/nexters/keyme/member/dto/request/LoginRequestDto.java
+++ src/main/java/com/nexters/keyme/member/dto/request/LoginRequestDto.java
@@ -1,19 +1,21 @@
 package com.nexters.keyme.member.dto.request;
 
 import com.nexters.keyme.common.enums.OAuthType;
-import lombok.AllArgsConstructor;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@ApiModel
 @Getter
 public class LoginRequestDto {
 
+  @ApiModelProperty(value = "Oauth 타입(KAKAO/APPLE)", example = "KAKAO", required = true)
   @NotNull(message = "유효하지 않는 OAuth 타입")
   private OAuthType oauthType;
 
+  @ApiModelProperty(value = "Oauth SDK를 통해 받은 access token(kakao)/identity token(apple)", example = "(token value)", required = true)
   @NotNull
   private String token;
 }
