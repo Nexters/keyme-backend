f2231fa0aaeaaaf7c7c209fafc61185e3d624008
RokwonK <wonrok97@naver.com>
2023-07-24 00:22:39 +0900

    🎨 header public으로 변경

--- src/main/java/com/nexters/keyme/common/config/WebMvcConfig.java
+++ src/main/java/com/nexters/keyme/common/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import com.nexters.keyme.auth.resolver.UserInfoArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,7 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    static final String AUTHORIZATION_HEADER = "Autorization";
+    public static final String AUTHORIZATION_HEADER = "Autorization";
     private final UserInfoArgumentResolver userInfoArgumentResolver;
 
     @Override
