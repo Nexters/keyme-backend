ee03c3d667e343859a6e97a75e24a962c9e7ad83
RokwonK <wonrok97@naver.com>
2023-07-24 00:21:43 +0900

    ✨ DummyController Swagger 설명 추가

--- src/main/java/com/nexters/keyme/dummy/DummyController.java
+++ src/main/java/com/nexters/keyme/dummy/DummyController.java
@@ -1,25 +1,32 @@
 package com.nexters.keyme.dummy;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.nexters.keyme.common.config.SwaggerConfig.SWAGGER_AUTHORIZATION_SCHEME;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/hello")
+@Api(tags = "테스트", description = "")
 public class DummyController {
     
     private final DummyService dummyService;
 
-    @GetMapping("/hello")
-    @ApiOperation(value = "테스트용 컨트롤러(인증 불필요)", notes = "인증처리 없이 접근할 수 있는 엔드포인트")
+    @GetMapping
+    @ApiOperation(value = "(테스트용)")
     public String helloKeyme() {
-        return "Hello keyme!";
+        return "Hello keyme";
     }
 
     @GetMapping("/auth")
-    @ApiOperation(value = "Hello Auth API")
+    @ApiOperation(value = "(테스트용) 인증필요")
+    @SecurityRequirement(name = SWAGGER_AUTHORIZATION_SCHEME)
     public String helloAuthKeyme() {
         return "Hello Authenticated keyme!";
     }
