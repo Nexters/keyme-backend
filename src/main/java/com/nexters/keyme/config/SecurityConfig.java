2d5ddcec3406a9ea7ef903a3f128564a066020e6
RokwonK <wonrok97@naver.com>
2023-07-26 23:23:13 +0900

    🎨 Cors 셋팅 메서드 변경

--- src/main/java/com/nexters/keyme/config/SecurityConfig.java
+++ src/main/java/com/nexters/keyme/config/SecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
@@ -62,15 +65,14 @@ public class SecurityConfig {
 
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
-    UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
     CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.addAllowedOriginPattern("*");
-    configuration.addAllowedOrigin("*");
-    configuration.addAllowedHeader("*");
-    configuration.addAllowedMethod("HEAD, POST, GET, DELETE, PUT, PATCH, OPTIONS");
+    configuration.setAllowedOriginPatterns(List.of("*"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT", "PATCH", "OPTIONS"));
     configuration.setAllowCredentials(true);
 
+    UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
     configurationSource.registerCorsConfiguration("/**", configuration);
     return configurationSource;
   }
