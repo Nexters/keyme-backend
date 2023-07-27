05d86b5fe4ea9f08510dcbf7a6dfdd2eb6074980
Senglee Choi <tmdfl1027@gmail.com>
2023-07-27 22:58:41 +0900

    ✨ 전역 ErrorCode enum 정의
    HTTP 상태코드 기준 400, 401, 403, 404로 나누어 정의.

---
+++ src/main/java/com/nexters/keyme/common/exceptions/ErrorCode.java
@@ -0,0 +1,31 @@
+package com.nexters.keyme.common.exceptions;
+
+public enum ErrorCode {
+    INVALID_REQUEST(400, "올바르지 않은 요청입니다. 요청 형식 등을 확인하세요."),
+
+    TOKEN_EXPIRED(401, "액세스 토큰이 만료되었습니다."),
+    ACCESS_TOKEN_NOT_EXIST(401, "액세스 토큰이 존재하지 않습니다."),
+    REFRESH_TOKEN_NOT_EXIST(401, "리프레시 토큰이 존재하지 않습니다."),
+    TOKEN_INVALID(401, "올바르지 않은 토큰입니다."),
+    UNAUTHORIZED(401, "인증되지 않은 사용자입니다."),
+
+    ACCESS_DENIED(403, "접근 권한이 없습니다."),
+
+    RESOURCE_NOT_FOUND(404, "대상을 찾을 수 없습니다.");
+
+    private final int statusCode;
+    private final String message;
+
+    ErrorCode(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public int statusCode() {
+        return statusCode;
+    }
+
+    public String message() {
+        return message;
+    }
+}
