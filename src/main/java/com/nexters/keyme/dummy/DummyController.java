5a2699bcb2a644f61ed64cdda947540484d1f0ff
RokwonK <wonrok97@naver.com>
2023-07-28 17:04:26 +0900

    ✨ 인포, 에러 더미 로그 추가

--- src/main/java/com/nexters/keyme/dummy/DummyController.java
+++ src/main/java/com/nexters/keyme/dummy/DummyController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import static com.nexters.keyme.config.SwaggerConfig.SWAGGER_AUTHORIZATION_SCHEM
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/hello")
+@Slf4j
 @Api(tags = "테스트", description = "")
 public class DummyController {
     
@@ -21,7 +23,15 @@ public class DummyController {
     @GetMapping
     @ApiOperation(value = "(테스트용)")
     public String helloKeyme() {
+        log.info("test tset");
         return "Hello keyme";
+    }
+
+    @GetMapping("/error")
+    @ApiOperation(value = "(테스트용)")
+    public String helloError() {
+        log.error("error");
+        return "Hello Error";
     }
 
     @GetMapping("/auth")
