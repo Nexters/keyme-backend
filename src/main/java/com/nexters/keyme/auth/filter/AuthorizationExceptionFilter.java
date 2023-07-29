7af81ae428000675d18836e12448300d559b8f16
Senglee Choi <tmdfl1027@gmail.com>
2023-07-29 12:59:40 +0900

    ✨ AuthorizationExceptionFilter 작성
    인증 관련 예외 발생 시 ErrorCode에 지정된 메시지 반환

---
+++ src/main/java/com/nexters/keyme/auth/filter/AuthorizationExceptionFilter.java
@@ -0,0 +1,34 @@
+package com.nexters.keyme.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nexters.keyme.common.dto.CommonResponse;
+import com.nexters.keyme.common.exceptions.AuthorizationFailedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class AuthorizationExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (AuthorizationFailedException e) {
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+            objectMapper.writeValue(response.getWriter(), new CommonResponse(HttpStatus.UNAUTHORIZED.value(), e.getMessage()));
+        }
+    }
+}
