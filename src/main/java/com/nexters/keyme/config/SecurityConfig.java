7c6ca26558dfc08cd0c04fd792a00af6587bb9e5
RokwonK <wonrok97@naver.com>
2023-08-02 10:00:50 +0900

    ✨ error 로그 테스트 path 오픈

--- src/main/java/com/nexters/keyme/config/SecurityConfig.java
+++ src/main/java/com/nexters/keyme/config/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
             .permitAll()
             .antMatchers(
                     "/hello",
+                    "/hello/error",
                     "/auth/login"
             )
             .permitAll()
