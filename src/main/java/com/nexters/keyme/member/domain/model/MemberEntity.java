26a5482836c802e374170df38fe14991fd56110c
RokwonK <wonrok97@naver.com>
2023-08-02 20:03:36 +0900

    🎨 config 업데이트

--- src/main/java/com/nexters/keyme/member/domain/model/MemberEntity.java
+++ src/main/java/com/nexters/keyme/member/domain/model/MemberEntity.java
@@ -1,9 +1,13 @@
 package com.nexters.keyme.member.domain.model;
 
 import com.nexters.keyme.common.domain.BaseTimeEntity;
+import com.nexters.keyme.question.domain.QuestionSolved;
+import com.nexters.keyme.test.domain.Test;
+import com.nexters.keyme.test.domain.TestResultSimpleInfo;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -18,6 +22,15 @@ public class MemberEntity extends BaseTimeEntity {
     private List<OauthInfo> oauthInfo;
     @OneToMany(fetch = FetchType.LAZY)
     private List<DeviceInfo> deviceInfo;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<Test> testList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<QuestionSolved> questionSolvedList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<TestResultSimpleInfo> testResultSimpleInfoList = new ArrayList<>();
 
     private String nickname;
     private String inviteCode;
