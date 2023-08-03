b06863bb7c5b64897702fd1b52a24b084cca7d32
Senglee Choi <tmdfl1027@gmail.com>
2023-08-03 22:20:43 +0900

    ✨ MemberRepository 테스트 추가
    - MemberRepository에서 직접 정의한 메서드의 테스트 추가

---
+++ src/test/java/com/nexters/keyme/member/domain/repository/MemberRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.nexters.keyme.member.domain.repository;
+
+import com.nexters.keyme.common.exceptions.ResourceNotFoundException;
+import com.nexters.keyme.member.domain.model.MemberEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DataJpaTest
+@AutoConfigureTestDatabase
+@ActiveProfiles("test")
+class MemberRepositoryTest {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        MemberEntity member = MemberEntity.builder()
+                .id(1L)
+                .nickname("nick")
+                .inviteCode("AZ03A20")
+                .build();
+
+        memberRepository.save(member);
+    }
+
+    @Test
+    @DisplayName("닉네임으로 멤버 엔티티 찾기 테스트")
+    void findByNickname() {
+        assertThatThrownBy(() -> {
+            memberRepository.findByNickname("james").orElseThrow(ResourceNotFoundException::new);
+        }).isInstanceOf(ResourceNotFoundException.class);
+
+        MemberEntity member = memberRepository.findByNickname("nick")
+                .orElseThrow(ResourceNotFoundException::new);
+
+        assertThat(member.getNickname()).isEqualTo("nick");
+    }
+
+    @Test
+    @DisplayName("초대코드로 멤버 엔티티 찾기 테스트")
+    void findByInviteCode() {
+        assertThatThrownBy(() -> {
+            memberRepository.findByInviteCode("1234567").orElseThrow(ResourceNotFoundException::new);
+        }).isInstanceOf(ResourceNotFoundException.class);
+
+        MemberEntity member = memberRepository.findByInviteCode("AZ03A20")
+                .orElseThrow(ResourceNotFoundException::new);
+
+        assertThat(member.getInviteCode()).isEqualTo("AZ03A20");
+    }
+}
