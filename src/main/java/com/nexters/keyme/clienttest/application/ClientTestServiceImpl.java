f68ea69afcf6153cbaafad4c5704b9100d509433
RokwonK <wonrok97@naver.com>
2023-08-06 18:11:48 +0900

    ✨ client test를 위한 API 프레임 추가

---
+++ src/main/java/com/nexters/keyme/clienttest/application/ClientTestServiceImpl.java
@@ -0,0 +1,10 @@
+package com.nexters.keyme.clienttest.application;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ClientTestServiceImpl implements ClientTestService {
+}
