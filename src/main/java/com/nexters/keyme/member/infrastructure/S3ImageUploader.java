5d47494ec4813f347cb2075941dba3601562a2d7
Senglee Choi <tmdfl1027@gmail.com>
2023-08-07 22:55:56 +0900

    ✨ S3ImageUploader 내 리사이징 로직 작성
    thumbnailator 라이브러리를 사용하여 리사이징

--- src/main/java/com/nexters/keyme/member/infrastructure/S3ImageUploader.java
+++ src/main/java/com/nexters/keyme/member/infrastructure/S3ImageUploader.java
@@ -6,13 +6,13 @@ import com.nexters.keyme.member.domain.internaldto.ImageInfo;
 import com.nexters.keyme.member.domain.service.ImageUploader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.util.Objects;
 import java.util.UUID;
 
 @Component
@@ -20,30 +20,68 @@ import java.util.UUID;
 @Slf4j
 public class S3ImageUploader implements ImageUploader {
     private final AmazonS3Client s3Client;
+    @Value("${image-temp}")
+    private String tempImagePath;
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
     @Override
     public ImageInfo uploadImage(MultipartFile image) {
-        try {
-            String originalUrl = uploadToS3(image.getInputStream(), new ObjectMetadata());
-            String thumbnailUrl = uploadToS3(new FileInputStream(resizeForThumbnail(image)), new ObjectMetadata());
+        String extension = extractExtension(image);
+        String originalUrl;
+        String thumbnailUrl;
 
-            return new ImageInfo(originalUrl, thumbnailUrl);
-        } catch (Exception e) {
+        try {
+            originalUrl = uploadToS3(image.getInputStream(), new ObjectMetadata(), extension);
+            thumbnailUrl = uploadToS3(new FileInputStream(resizeForThumbnail(image)), new ObjectMetadata(), extension);
+        } catch (IOException e) {
             log.info(e.getMessage());
             throw new RuntimeException();
         }
+
+        return new ImageInfo(originalUrl, thumbnailUrl);
     }
 
     private File resizeForThumbnail(MultipartFile image) {
-        return new File("dummy path");
+        String extension = extractExtension(image);
+
+        File original = new File(tempImagePath + UUID.randomUUID() + "." + extension);
+        File thumbnail = new File(tempImagePath + UUID.randomUUID() + "." + extension);
+
+        try (FileOutputStream os = new FileOutputStream(original)) {
+            os.write(image.getBytes());
+        } catch (IOException e) {
+            log.info(e.getMessage());
+            throw new RuntimeException();
+        }
+
+        try {
+            Thumbnails.of(original)
+                .size(700, 400)
+                .toFile(thumbnail);
+        } catch (IOException e) {
+            log.info(e.getMessage());
+            throw new RuntimeException();
+        }
+
+        original.delete();
+
+        return thumbnail;
     }
 
-    private String uploadToS3(InputStream inputStream, ObjectMetadata metadata) {
-        String key = UUID.randomUUID() + ".jpg";
+    private String uploadToS3(InputStream inputStream, ObjectMetadata metadata, String extension) {
+        String key = UUID.randomUUID() + "." + extension;
 
         s3Client.putObject(bucket, key, inputStream, metadata);
         return s3Client.getUrl(bucket, key).toString();
+    }
+
+    private String extractExtension(MultipartFile image) {
+        return Objects.requireNonNull(image.getContentType()).split("/")[1];
+    }
+
+    private void clearFiles(File original, File thumbnail) {
+        original.delete();
+        thumbnail.delete();
     }
 }
