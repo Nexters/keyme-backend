476178b522a2d43417b3d297bbf31d2a5ffafcba
RokwonK <wonrok97@naver.com>
2023-08-07 23:59:01 +0900

    ✨ test helper 추가

--- src/main/java/com/nexters/keyme/test/domain/helper/TestResultAsyncHelper.java
+++ src/main/java/com/nexters/keyme/test/domain/helper/TestResultAsyncHelper.java
@@ -1,13 +1,25 @@
 package com.nexters.keyme.test.domain.helper;
 
-import com.nexters.keyme.question.domain.repository.QuestionRepository;
+import com.nexters.keyme.test.domain.model.TestResult;
 import com.nexters.keyme.test.domain.repository.TestResultRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 @RequiredArgsConstructor
-public class TestAsyncHelper {
+public class TestResultAsyncHelper {
     private final TestResultRepository testResultRepository;
 
+    @Async
+    public CompletableFuture<Optional<TestResult>> asyncFindByTestIdAndSolverId(Long testId, Long solvedMemberId) {
+        if (solvedMemberId == null) return null;
+
+        Optional<TestResult> testResult = testResultRepository.findByTestIdAndSolverId(testId, solvedMemberId);
+
+        return CompletableFuture.completedFuture(testResult);
+    }
 }
