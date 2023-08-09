16ea805c02b1ecd87dd2b872b0f2cc8613ec0391
RokwonK <wonrok97@naver.com>
2023-08-09 12:57:16 +0900

    ✨ QuestionSolvedRepository 내 최근에 푼 문제 가져오기, 푼 문제 묶음, 통계 쿼리 추가

--- src/main/java/com/nexters/keyme/question/domain/repository/QuestionSolvedRepository.java
+++ src/main/java/com/nexters/keyme/question/domain/repository/QuestionSolvedRepository.java
@@ -1,7 +1,34 @@
 package com.nexters.keyme.question.domain.repository;
 
+import com.nexters.keyme.question.domain.internaldto.QuestionStatisticInfo;
 import com.nexters.keyme.question.domain.model.QuestionSolved;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface QuestionSolvedRepository extends JpaRepository<QuestionSolved, Long> {
+
+    @Query(
+        "select qs from QuestionSolved qs " +
+            "where qs.testResult.solver.id = :solverId and qs.testResult.test.member.id = :ownerId " +
+            "order by qs.createdAt DESC "
+    )
+    List<QuestionSolved> findFirstLastestQuestionSolved(Long solverId, Long ownerId);
+
+    @Query(
+        "select qs from QuestionSolved qs " +
+            "join fetch qs.question " +
+            "where qs.testResult.testResultId = :resultId"
+    )
+    List<QuestionSolved> findAllByTestResultIdWithQuestion(Long resultId);
+
+    @Query(
+        "select new com.nexters.keyme.question.domain.internaldto.QuestionStatisticInfo(qs.question.questionId, qs.question.description, qs.question.keyword, qs.question.categoryName, avg(qs.score)) " +
+            "from QuestionSolved qs " +
+            "where qs.testResult.test.testId = :testId " +
+            "group by qs.question.questionId "
+    )
+    List<QuestionStatisticInfo> findAllAssociatedQuestionStatisticsByTestId(Long testId);
 }
