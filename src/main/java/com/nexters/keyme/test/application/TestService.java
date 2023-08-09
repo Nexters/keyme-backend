78f5fd8751395ccfaecb4d9bebd9d196215f335f
RokwonK <wonrok97@naver.com>
2023-08-09 12:58:30 +0900

    🎨 test service interface 수정

--- src/main/java/com/nexters/keyme/test/application/TestService.java
+++ src/main/java/com/nexters/keyme/test/application/TestService.java
@@ -2,17 +2,16 @@ package com.nexters.keyme.test.application;
 
 import com.nexters.keyme.test.presentation.dto.request.TestListRequest;
 import com.nexters.keyme.test.presentation.dto.request.TestSubmissionRequest;
-import com.nexters.keyme.test.presentation.dto.response.QuestionsInTestResponse;
-import com.nexters.keyme.test.presentation.dto.response.TestFeedResponse;
-import com.nexters.keyme.test.presentation.dto.response.TestResultResponse;
+import com.nexters.keyme.test.presentation.dto.response.*;
 
 import java.util.List;
 
 public interface TestService {
-    QuestionsInTestResponse getDailyTest(Long memberId);
-    QuestionsInTestResponse getOnboardingTest();
-    QuestionsInTestResponse getSpecificTest(Long memberId, Long testId);
+    TestDetailResponse getOrCreateDailyTest(Long memberId);
+    TestDetailResponse getOrCreateOnboardingTest(Long memberId);
+    TestDetailResponse getSpecificTest(Long memberId, Long testId);
+    SingleTestStatisticsResponse getTestStatistics(Long memberId, Long testId);
     List<TestFeedResponse> getTestList(Long memberId, TestListRequest requestInfo);
-    TestResultResponse submitTest(Long solverId, Long testId, TestSubmissionRequest submitInfo);
-    TestResultResponse getTestResult(Long solverId, Long testId);
+    TestSubmitResponse createTestResult(Long solvedMemberId, Long testId, TestSubmissionRequest submitInfo);
+    TestResultResponse getTestResult(Long testId, Long resultId);
 }
