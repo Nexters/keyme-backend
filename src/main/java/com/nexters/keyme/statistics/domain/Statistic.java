4562249c0c188fd298ef980388b62c65c8586e95
Senglee Choi <tmdfl1027@gmail.com>
2023-08-10 20:28:19 +0900

    ✨ Statistic id auto-increment 추가

--- src/main/java/com/nexters/keyme/statistics/domain/Statistic.java
+++ src/main/java/com/nexters/keyme/statistics/domain/Statistic.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -13,6 +15,7 @@ import javax.persistence.Id;
 @Getter
 public class Statistic {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private long ownerId;
     private long questionId;
