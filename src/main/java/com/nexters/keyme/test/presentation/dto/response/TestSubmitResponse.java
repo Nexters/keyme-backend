d0ee3459db1fb840f654b36fd20fa99e22904292
RokwonK <wonrok97@naver.com>
2023-08-11 17:24:05 +0900

    🎨 TestSubmitResponse 내 resultCode 추가

--- src/main/java/com/nexters/keyme/test/presentation/dto/response/TestSubmitResponse.java
+++ src/main/java/com/nexters/keyme/test/presentation/dto/response/TestSubmitResponse.java
@@ -16,6 +16,9 @@ import lombok.NoArgsConstructor;
 public class TestSubmitResponse {
     private Long testResultId;
 
+    @ApiModelProperty(value = "익명유저의 제출에서만 존재(6자리)", example = "Wqa3dk")
+    private String resultCode;
+
     @ApiModelProperty(value = "테스트 제출자와의 일치율(MMVP에서는 없음)", example = "87.7")
     private float matchRate;
 }
