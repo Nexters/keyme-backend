0c2ed3d5b0cf213a5f489eb37a2b89f8ee2ac6f9
RokwonK <wonrok97@naver.com>
2023-08-13 04:35:58 +0900

    ✨ Timezone Seoul로 설정 추가

--- src/main/java/com/nexters/keyme/common/config/JpaConfig.java
+++ src/main/java/com/nexters/keyme/common/config/JpaConfig.java
@@ -3,7 +3,14 @@ package com.nexters.keyme.common.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
+    @PostConstruct
+    public void setSeoulTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 }
