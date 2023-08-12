2ecf576af5ea1aa645493d0764f72c227b0f899f
Senglee Choi <tmdfl1027@gmail.com>
2023-08-12 20:13:46 +0900

    ✨ Python 좌표변환 서비스 더미 응답값 추가

--- src/main/java/com/nexters/keyme/statistics/infrastcurture/PythonCoordinateConversionService.java
+++ src/main/java/com/nexters/keyme/statistics/infrastcurture/PythonCoordinateConversionService.java
@@ -11,6 +11,12 @@ import java.util.List;
 public class PythonCoordinateConversionService implements CoordinateConversionService {
     @Override
     public List<CoordinateInfo> convertFrom(List<Statistic> statistics) {
-        return null;
+        CoordinateInfo info1 = new CoordinateInfo(1, 1, 1);
+        CoordinateInfo info2 = new CoordinateInfo(1, 1, 1);
+        CoordinateInfo info3 = new CoordinateInfo(1, 1, 1);
+        CoordinateInfo info4 = new CoordinateInfo(1, 1, 1);
+        CoordinateInfo info5 = new CoordinateInfo(1, 1, 1);
+
+        return List.of(info1, info2, info3, info4, info5);
     }
 }
