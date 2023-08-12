fb54ff192bd396abfb69d2819809266e212ae043
Senglee Choi <tmdfl1027@gmail.com>
2023-08-12 19:32:25 +0900

    🎨 통계 목록 조회 시 LIMIT 절 사용을 위해 nativeQuery 사용으로 변경

--- src/main/java/com/nexters/keyme/statistics/domain/repository/StatisticRepository.java
+++ src/main/java/com/nexters/keyme/statistics/domain/repository/StatisticRepository.java
@@ -2,20 +2,23 @@ package com.nexters.keyme.statistics.domain.repository;
 
 import com.nexters.keyme.statistics.domain.model.Statistic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
 public interface StatisticRepository extends JpaRepository<Statistic, Long> {
 
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT st FROM Statistic st WHERE st.ownerId = :ownerId AND st.questionId = :questionId")
     Optional<Statistic> findByOwnerIdAndQuestionIdWithLock(long ownerId, long questionId);
 
-    @Query("SELECT st FROM Statistic st WHERE st.ownerId = :memberId ORDER BY st.matchRate")
+    @Query(value = "SELECT * FROM statistic st WHERE st.owner_id = :memberId ORDER BY st.match_rate LIMIT 5", nativeQuery = true)
     List<Statistic> findByMemberIdSortByMatchRateAsc(long memberId);
 
-    @Query("SELECT st FROM Statistic st WHERE st.ownerId = :memberId ORDER BY st.matchRate DESC")
+    @Query(value = "SELECT * FROM statistic st WHERE st.owner_id = :memberId ORDER BY st.match_rate DESC LIMIT 5", nativeQuery = true)
     List<Statistic> findByMemberIdSortByMatchRateDesc(long memberId);
 
 //    @Query("SELECT st FROM Statistic st WHERE st.ownerId = :memberId ORDER BY st.matchRate")
