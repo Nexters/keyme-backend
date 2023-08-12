3022072cdf8dcf6e82547bd14e401b96a90b17f8
RokwonK <wonrok97@naver.com>
2023-08-13 04:46:02 +0900

    🎨 EntiryPoint에 집중된 Config 파일로 분리

--- src/main/java/com/nexters/keyme/KeymeApplication.java
+++ src/main/java/com/nexters/keyme/KeymeApplication.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableAsync
 public class KeymeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KeymeApplication.class, args);
