3e619fbb85d0c709a2724dfb450a100db8f5458d
RokwonK <wonrok97@naver.com>
2023-08-15 00:05:32 +0900

    ✨ Question Solved Statistics 관련 테스트 추가

--- src/test/java/com/nexters/keyme/question/domain/repository/QuestionSolvedRepositoryTest.java
+++ src/test/java/com/nexters/keyme/question/domain/repository/QuestionSolvedRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.nexters.keyme.question.domain.repository;
 
 import com.nexters.keyme.common.config.QueryDslConfig;
+import com.nexters.keyme.question.domain.internaldto.QuestionStatisticInfo;
 import com.nexters.keyme.question.domain.model.QuestionSolved;
 import com.nexters.keyme.test.annotation.RepositoryTest;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Page;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,7 +27,7 @@ class QuestionSolvedRepositoryTest {
     private QuestionSolvedRepository questionSolvedRepository;
 
     @Test
-    @DisplayName("질문을 푼 사람들의 점수 리스트 받아오기")
+    @DisplayName("해당 문제플 푼 사람들의 점수를 가져온다")
     void findQuestionSolvedList() {
         Long questionId = 1L;
         Long questionOwnerId = 1L;
@@ -42,5 +44,29 @@ class QuestionSolvedRepositoryTest {
             () -> assertThat(questionSolvedIdList.get(0)).isEqualTo(21),
             () -> assertThat(questionSolvedList.getContent().size()).isEqualTo(2)
         );
+    }
+
+    @Test
+    @DisplayName("Test이 속해있는 모든 Quesetion들의 통계를 가져온다")
+    void findAllAssociatedQuestionStatisticsByTestId() {
+        Long testId = 1L;
+        List<QuestionStatisticInfo> questionStatisticInfoList = questionSolvedRepository.findAllAssociatedQuestionStatisticsByTestId(testId);
+
+        assertAll(
+            () -> assertThat(questionStatisticInfoList.get(0).getQuestionId()).isEqualTo(1),
+            () -> assertThat(questionStatisticInfoList.get(0).getAverageScore()).isEqualTo(2.0),
+            () -> assertThat(questionStatisticInfoList.size()).isEqualTo(10)
+        );
+    }
+
+    @Test
+    @DisplayName("특정 Question의 통계를 가져온다.")
+    void findQuestionStatisticsByQuestionIdAndOwnerId() {
+        Long questionId = 1L;
+        Long ownerId = 1L;
+
+        QuestionStatisticInfo questionStatisticInfo = questionSolvedRepository.findQuestionStatisticsByQuestionIdAndOwnerId(questionId, ownerId).orElse(null);
+
+        assertThat(questionStatisticInfo.getAverageScore()).isEqualTo(2.0);
     }
 }
