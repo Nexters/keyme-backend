3210b813d28f5bb1eaa7c5835f4c469bd7c6ffde
Senglee Choi <tmdfl1027@gmail.com>
2023-08-14 19:49:43 +0900

    ✨ 멤버 성격 더보기 컨트롤러 추가
    memberId를 path variable로 받고, cursorId를 parameter로 받아 처리

--- src/main/java/com/nexters/keyme/statistics/presentation/controller/StatisticController.java
+++ src/main/java/com/nexters/keyme/statistics/presentation/controller/StatisticController.java
@@ -2,6 +2,7 @@ package com.nexters.keyme.statistics.presentation.controller;
 
 import com.nexters.keyme.common.dto.response.ApiResponse;
 import com.nexters.keyme.statistics.application.StatisticService;
+import com.nexters.keyme.statistics.presentation.dto.AdditionalStatisticResponse;
 import com.nexters.keyme.statistics.presentation.dto.request.StatisticRequest;
 import com.nexters.keyme.statistics.presentation.dto.response.MemberStatisticResponse;
 import io.swagger.annotations.Api;
@@ -11,7 +12,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import static com.nexters.keyme.common.config.SwaggerConfig.SWAGGER_AUTHORIZATION_SCHEME;
 
@@ -26,6 +30,15 @@ public class StatisticController {
     public ResponseEntity<ApiResponse<MemberStatisticResponse>> getMemberStatistic(@PathVariable(name = "memberId") Long memberId,
                                                                                    StatisticRequest request) {
         MemberStatisticResponse response = statisticService.getMemberStatistic(memberId, request);
+        return ResponseEntity.ok(new ApiResponse<>(response));
+    }
+
+    @GetMapping("/members/{memberId}/statistics/more")
+    @ApiOperation(value = "멤버의 성격 더보기")
+    @SecurityRequirement(name = SWAGGER_AUTHORIZATION_SCHEME)
+    public ResponseEntity<ApiResponse<List<AdditionalStatisticResponse>>> getMemberStatistic(@PathVariable(name = "memberId") Long memberId,
+                                                                                             @RequestParam long cursorId) {
+        List<AdditionalStatisticResponse> response = statisticService.getAdditionalStatistics(memberId, cursorId);
         return ResponseEntity.ok(new ApiResponse<>(response));
     }
 }
