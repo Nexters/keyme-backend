4f80d948b04e61f129fe909f9e8a21940fe74d34
Senglee Choi <tmdfl1027@gmail.com>
2023-08-14 23:17:31 +0900

    🎨 멤버 성격 더보기 응답 API 설명 작성

--- src/main/java/com/nexters/keyme/statistics/presentation/dto/AdditionalStatisticResponse.java
+++ src/main/java/com/nexters/keyme/statistics/presentation/dto/AdditionalStatisticResponse.java
@@ -1,5 +1,6 @@
 package com.nexters.keyme.statistics.presentation.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public class AdditionalStatisticResponse {
+    @ApiModelProperty(value="통계 정보 id", example = "1")
     private final long statisticId;
+    @ApiModelProperty(value="문제 키워드", example = "소통왕")
     private final String keyword;
+    @ApiModelProperty(value="푼 사람들의 평균 점수", example = "1.5")
     private final double solverAvgScore;
 }
