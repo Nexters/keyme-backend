faefe1b889e2f2364bff504ca6a7ae32bc3065de
RokwonK <wonrok97@naver.com>
2023-08-16 00:16:06 +0900

    ✨ loggingTraceFilter 추가

---
+++ src/main/java/com/nexters/keyme/common/filter/LoggingTraceFilter.java
@@ -0,0 +1,25 @@
+package com.nexters.keyme.common.filter;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class LoggingTraceFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        MDC.put("traceId", UUID.randomUUID().toString());
+        MDC.put("requestUri", request.getRequestURI());
+
+        filterChain.doFilter(request, response);
+
+        MDC.clear();
+    }
+}
