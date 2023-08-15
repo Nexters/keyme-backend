403a13eb6948f081c866ee40a5b055db23a3e3b6
Senglee Choi <tmdfl1027@gmail.com>
2023-08-15 16:42:47 +0900

    🎨 MemberDeviceRepository 및 테스트 추가, 더미 SQL 추가

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -7,6 +7,17 @@ VALUES
     (2, 'ACTIVE', 'james', '1234567', 'keyme.space/original', 'keyme.space.thumbnail', '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
     (3, 'ACTIVE', 'peter', 'A1B2C3D', 'keyme.space/original', 'keyme.space.thumbnail', '2023-08-05T16:07:00', '2023-08-05T16:07:00');
 
+
+/*
+    Member Device 정보 추가
+*/
+INSERT INTO member_device (member_device_id, member_id, token)
+VALUES
+    (1, 1, 'token1'),
+    (2, 1, 'token2'),
+    (3, 1, 'token3');
+
+
 /*
     Question 추가
 */
