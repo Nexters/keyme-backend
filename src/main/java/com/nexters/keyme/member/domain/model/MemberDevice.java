da0b87c5d40b5d6e60483b5babd497ca344d085f
Senglee Choi <tmdfl1027@gmail.com>
2023-08-15 16:40:24 +0900

    🎨 MemberDevice 생성자 및 빌더 추가

--- src/main/java/com/nexters/keyme/member/domain/model/MemberDevice.java
+++ src/main/java/com/nexters/keyme/member/domain/model/MemberDevice.java
@@ -1,12 +1,18 @@
 package com.nexters.keyme.member.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "member_device")
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class MemberDevice {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
