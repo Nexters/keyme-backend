0672d662f2d9a90a5974b8e35ddcf1eca6b752a0
RokwonK <wonrok97@naver.com>
2023-08-16 12:22:18 +0900

    ✨ 요청 로깅 filter 추가

--- src/main/java/com/nexters/keyme/common/config/FilterConfig.java
+++ src/main/java/com/nexters/keyme/common/config/FilterConfig.java
@@ -1,7 +1,9 @@
 package com.nexters.keyme.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nexters.keyme.common.filter.LoggingFilter;
 import com.nexters.keyme.common.filter.LoggingTraceFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +12,10 @@ import javax.servlet.Filter;
 import java.util.Arrays;
 
 @Configuration
+@RequiredArgsConstructor
 public class FilterConfig {
+
+    private final ObjectMapper objectMapper;
 
     @Bean
     public FilterRegistrationBean registerLoggingTracerFilter() {
@@ -24,7 +29,7 @@ public class FilterConfig {
     @Bean
     public FilterRegistrationBean registerLoggingFilter() {
         FilterRegistrationBean<Filter> filter = new FilterRegistrationBean<>();
-        filter.setFilter(new LoggingFilter());
+        filter.setFilter(new LoggingFilter(objectMapper));
         filter.setOrder(2);
         filter.setUrlPatterns(Arrays.asList("/*"));
         return filter;
