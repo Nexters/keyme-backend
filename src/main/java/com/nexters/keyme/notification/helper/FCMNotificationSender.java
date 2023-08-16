b9441c7273b9f842a355b47389f9fac27f703fa8
Senglee Choi <tmdfl1027@gmail.com>
2023-08-16 18:41:35 +0900

    ✨ FCMNotificationSender 토큰이 0개일 때 예외처리
    유저의 등록된 토큰이 없어 토큰이 0개 -> Message가 0개일 때 리턴 처리

--- src/main/java/com/nexters/keyme/notification/helper/FCMNotificationSender.java
+++ src/main/java/com/nexters/keyme/notification/helper/FCMNotificationSender.java
@@ -1,7 +1,11 @@
 package com.nexters.keyme.notification.helper;
 
-import com.google.firebase.messaging.*;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
+@Slf4j
 public class FCMNotificationSender implements NotificationSender {
 
     private final FirebaseMessaging firebaseMessaging;
@@ -30,10 +35,14 @@ public class FCMNotificationSender implements NotificationSender {
                                 .build())
                 .collect(Collectors.toList());
 
+        if (messages.size() == 0) {
+            return;
+        }
 
         try {
             firebaseMessaging.sendAll(messages);
-        } catch (FirebaseMessagingException e) {
+        } catch (Exception e) {
+            log.error(e.getMessage());
             throw new RuntimeException("메시지 발송 중 문제가 발생했습니다.");
         }
     }
