b410142f09308d4f2d4d9bdbe02a6d7be31f8b44
Senglee Choi <tmdfl1027@gmail.com>
2023-08-18 17:20:19 +0900

    🎨 Test 도메인 알림 발송 이벤트 수정, 자기 문제 풀이 시 예외처리
    - 변경된 notificationService에 맞게 단일 유저에게 보내는 인터페이스로 수정
    - TestEventHandler에서 solverId와 ownerId가 동일한 경우 알림 발송하지 않도록 수정
    - TestServiceImpl 에서 Long 객체 == 비교되어 있는 부분을 Objects.eqauls()로 수정

--- src/main/java/com/nexters/keyme/test/events/SendNotificationEvent.java
+++ src/main/java/com/nexters/keyme/test/events/SendNotificationEvent.java
@@ -4,11 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.List;
-
 @AllArgsConstructor
 @Builder
 @Getter
 public class SendNotificationEvent {
-    private List<Long> userIds;
+    private Long ownerId;
+    private Long solverId;
 }
