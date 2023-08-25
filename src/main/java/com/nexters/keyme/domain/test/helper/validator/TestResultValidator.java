778022ccff2557dc7457a410934140a714b3a536
RokwonK <wonrok97@naver.com>
2023-08-25 15:40:59 +0900

    ✨ TestResultAlreadyExist 로직 구현

--- src/main/java/com/nexters/keyme/domain/test/helper/validator/TestResultValidator.java
+++ src/main/java/com/nexters/keyme/domain/test/helper/validator/TestResultValidator.java
@@ -1,5 +1,9 @@
 package com.nexters.keyme.domain.test.helper.validator;
 
+import com.nexters.keyme.domain.member.domain.helper.validator.MemberValidator;
+import com.nexters.keyme.domain.member.domain.model.MemberEntity;
+import com.nexters.keyme.domain.test.domain.model.Test;
+import com.nexters.keyme.domain.test.exceptions.AlreadyExistTestResultException;
 import com.nexters.keyme.domain.test.exceptions.NotFoundTestResultException;
 import com.nexters.keyme.domain.test.domain.model.TestResult;
 import com.nexters.keyme.domain.test.domain.repository.TestResultRepository;
@@ -7,9 +11,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 public class TestResultValidator {
+
+    private final MemberValidator memberValidator;
     private final TestResultRepository testResultRepository;
 
     @Transactional(readOnly = true)
@@ -19,8 +27,13 @@ public class TestResultValidator {
     }
 
     @Transactional(readOnly = true)
-    public TestResult validateTestResultExist() {
-        // member와 Test
-        return null;
+    public void validateTestResultAlreadyExist(Long solverId, Test test) {
+        // 익명유저
+        if (solverId == null) return;
+
+        MemberEntity member = memberValidator.validateMember(solverId);
+        TestResult testResult = testResultRepository.findByTestAndSolver(test, member).orElse(null);
+
+        if (testResult != null) throw new AlreadyExistTestResultException();
     }
 }
