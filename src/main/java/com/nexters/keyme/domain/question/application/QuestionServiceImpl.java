0f9e8e851de67e1e6b9123feec19696f6988f1ee
RokwonK <wonrok97@naver.com>
2023-08-29 14:59:55 +0900

    🎨 Question Service 내 유효성 검증 로직 question validator로 수정

--- src/main/java/com/nexters/keyme/domain/question/application/QuestionServiceImpl.java
+++ src/main/java/com/nexters/keyme/domain/question/application/QuestionServiceImpl.java
@@ -1,6 +1,8 @@
 package com.nexters.keyme.domain.question.application;
 
+import com.nexters.keyme.domain.member.domain.service.validator.MemberValidator;
 import com.nexters.keyme.domain.member.exceptions.NotFoundMemberException;
+import com.nexters.keyme.domain.question.domain.service.validator.QuestionValidator;
 import com.nexters.keyme.domain.question.exceptions.NotFoundQuestionException;
 import com.nexters.keyme.domain.question.domain.model.Question;
 import com.nexters.keyme.domain.question.domain.model.QuestionSolved;
@@ -29,19 +31,23 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class QuestionServiceImpl implements QuestionService {
     private final QuestionRepository questionRepository;
+    private final QuestionValidator questionValidator;
     private final QuestionSolvedRepository questionSolvedRepository;
-    private final MemberRepository memberRepository;
+
+    private final MemberValidator memberValidator;
+
 
     @Override
     public QuestionResponse getQuestion(Long questionId) {
-        Question question = questionRepository.findById(questionId).orElseThrow(NotFoundQuestionException::new);
+        Question question = questionValidator.validateQuestion(questionId);
         return new QuestionResponse(question);
     }
 
     @Override
     public QuestionStatisticResponse getQuestionStatistic(Long questionId, QuestionStatisticRequest request) {
-        Question question = questionRepository.findById(questionId).orElseThrow(NotFoundQuestionException::new);
-        MemberEntity member = memberRepository.findById(request.getOwnerId()).orElseThrow(NotFoundMemberException::new);
+        questionValidator.validateQuestion(questionId);
+        memberValidator.validateMember(request.getOwnerId());
+
         QuestionStatisticInfo questionStatisticInfo = questionSolvedRepository.findQuestionStatisticsByQuestionIdAndOwnerId(questionId, request.getOwnerId());
 
         return new QuestionStatisticResponse(questionStatisticInfo);
@@ -49,8 +55,8 @@ public class QuestionServiceImpl implements QuestionService {
 
     @Override
     public PageResponse<QuestionScoreInfoResponse> getQuestionSolvedList(Long questionId, QuestionScoreListRequest request) {
-        Question question = questionRepository.findById(questionId).orElseThrow(NotFoundQuestionException::new);
-        MemberEntity member = memberRepository.findById(request.getOwnerId()).orElseThrow(NotFoundMemberException::new);
+        questionValidator.validateQuestion(questionId);
+        memberValidator.validateMember(request.getOwnerId());
 
         PageInfo<QuestionSolved> solvedPage = questionSolvedRepository.findQuestionSolvedList(questionId, request.getOwnerId(), request.getCursor(), request.getLimit());
 
@@ -66,12 +72,13 @@ public class QuestionServiceImpl implements QuestionService {
     @Override
     @Deprecated
     public List<QuestionScoreInfoResponse> getQuestionSolvedScore(QuestionListScoreRequest request) {
+        memberValidator.validateMember(request.getOwnerId());
+
         List<Question> question = questionRepository.findAllById(request.getIds());
         if (question.size() != request.getIds().size()) {
             throw new NotFoundQuestionException();
         }
 
-        MemberEntity member = memberRepository.findById(request.getOwnerId()).orElseThrow(NotFoundMemberException::new);
         List<QuestionSolved> questionSolvedList = questionSolvedRepository.findByQuestionIdsAndOwnerIdAndSolverId(request.getIds(), request.getOwnerId(), request.getSolverId());
 
         return questionSolvedList.stream()
