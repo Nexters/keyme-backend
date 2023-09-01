144fe72edbeb46fa974c6014918a5ffbffa4f67d
RokwonK <wonrok97@naver.com>
2023-09-01 11:54:07 +0900

    ✨ Member 도메인 서비스 추가 - getmember, createmember, checkonboardingclear

---
+++ src/main/java/com/nexters/keyme/domain/member/domain/service/processor/MemberDataProcessor.java
@@ -0,0 +1,66 @@
+package com.nexters.keyme.domain.member.domain.service.processor;
+
+import com.nexters.keyme.domain.auth.dto.internal.OAuthUserInfo;
+import com.nexters.keyme.domain.member.domain.model.MemberEntity;
+import com.nexters.keyme.domain.member.domain.model.MemberOAuth;
+import com.nexters.keyme.domain.member.domain.model.MemberOAuthId;
+import com.nexters.keyme.domain.member.domain.model.ProfileImage;
+import com.nexters.keyme.domain.member.domain.repository.MemberOAuthRepository;
+import com.nexters.keyme.domain.member.domain.repository.MemberRepository;
+import com.nexters.keyme.domain.member.dto.response.MemberWithTokenResponse;
+import com.nexters.keyme.domain.test.domain.model.Test;
+import com.nexters.keyme.domain.test.domain.model.TestResult;
+import com.nexters.keyme.domain.test.domain.repository.TestResultRepository;
+import com.nexters.keyme.domain.test.domain.service.processor.TestDataProcessor;
+import com.nexters.keyme.global.common.enums.OAuthType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static com.nexters.keyme.global.common.constant.ConstantString.DEFAULT_IMAGE_URL;
+
+@Component
+@RequiredArgsConstructor
+public class MemberDataProcessor {
+
+    private final MemberOAuthRepository memberOAuthRepository;
+    private final MemberRepository memberRepository;
+    private final TestDataProcessor testDataProcessor;
+    private final TestResultRepository testResultRepository;
+
+    @Transactional
+    public Optional<MemberEntity> getMemberByOAuthId(OAuthType oauthType, String id) {
+        MemberOAuthId memberOAuthId = new MemberOAuthId(oauthType, id);
+        return memberOAuthRepository.findById(memberOAuthId)
+                .map(memberOAuth -> memberOAuth.getMember());
+    }
+
+    @Transactional
+    public MemberEntity createMember(OAuthType oauthType, String id) {
+        MemberOAuthId memberOAuthId = new MemberOAuthId(oauthType, id);
+        MemberEntity memberEntity = MemberEntity.builder()
+                .profileImage(new ProfileImage(DEFAULT_IMAGE_URL, DEFAULT_IMAGE_URL))
+                .build();
+        memberRepository.save(memberEntity);
+        memberOAuthRepository.save(
+                MemberOAuth.builder()
+                        .oauthInfo(memberOAuthId)
+                        .member(memberEntity)
+                        .build()
+        );
+
+        return memberEntity;
+    }
+
+    @Transactional
+    public Boolean checkOnboardingClear(MemberEntity member) {
+        Test onboardingTest = testDataProcessor.getExistOnboardingTest(member).orElseGet(null);
+        if (onboardingTest == null) return false;
+
+        TestResult testResult = testResultRepository.findByTestAndSolver(onboardingTest, member).orElseGet(null);
+
+        return testResult != null;
+    }
+}
