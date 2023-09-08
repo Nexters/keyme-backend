a9f8242aad89143d3f785aefa4a0fc542eefde76
Senglee Choi <tmdfl1027@gmail.com>
2023-09-08 18:36:16 +0900

    🎨 DeletedMemberException 생성자 인자 삭제
    - 기본 생성자를 통해 예외를 던질 수 있도록 함

--- src/main/java/com/nexters/keyme/domain/member/exceptions/DeletedMemberException.java
+++ src/main/java/com/nexters/keyme/domain/member/exceptions/DeletedMemberException.java
@@ -5,7 +5,7 @@ import com.nexters.keyme.global.common.exceptions.KeymeException;
 import org.springframework.http.HttpStatus;
 
 public class DeletedMemberException extends KeymeException {
-    public DeletedMemberException(HttpStatus httpStatus, String message, int code) {
+    public DeletedMemberException() {
         super(HttpStatus.OK, MemberErrorCode.DELETED_MEMBER.getMessage(), MemberErrorCode.DELETED_MEMBER.getCode());
     }
 }
