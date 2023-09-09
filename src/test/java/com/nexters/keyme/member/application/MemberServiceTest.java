170f13f3d7e542782aba1dd2535d2125faf49769
Senglee Choi <tmdfl1027@gmail.com>
2023-09-09 10:48:22 +0900

    🎨 멤버 삭제 통합테스트 is_deleted 필드 검증 추가

--- src/test/java/com/nexters/keyme/member/application/MemberServiceTest.java
+++ src/test/java/com/nexters/keyme/member/application/MemberServiceTest.java
@@ -1,15 +1,16 @@
 package com.nexters.keyme.member.application;
 
+import com.nexters.keyme.domain.member.application.MemberService;
+import com.nexters.keyme.domain.member.domain.model.MemberEntity;
+import com.nexters.keyme.domain.member.domain.repository.MemberRepository;
 import com.nexters.keyme.domain.member.domain.service.validator.MemberValidator;
+import com.nexters.keyme.domain.member.dto.request.MemberModificationRequest;
+import com.nexters.keyme.domain.member.dto.request.NicknameVerificationRequest;
+import com.nexters.keyme.domain.member.dto.response.MemberResponse;
+import com.nexters.keyme.domain.member.dto.response.NicknameVerificationResponse;
 import com.nexters.keyme.domain.member.exceptions.DeletedMemberException;
 import com.nexters.keyme.domain.member.exceptions.NicknameDuplicateException;
 import com.nexters.keyme.global.common.dto.internal.UserInfo;
-import com.nexters.keyme.domain.member.application.MemberService;
-import com.nexters.keyme.domain.member.dto.request.MemberModificationRequest;
-import com.nexters.keyme.domain.member.dto.request.NicknameVerificationRequest;
-import com.nexters.keyme.domain.member.dto.response.NicknameVerificationResponse;
-import com.nexters.keyme.domain.member.dto.response.MemberResponse;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,9 @@ public class MemberServiceTest {
 
     @Autowired
     private MemberValidator memberValidator;
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     @DisplayName("멤버 정보 가져오기 통합테스트")
@@ -74,8 +78,12 @@ public class MemberServiceTest {
     @Test
     @DisplayName("멤버 삭제 통합테스트")
     void deleteMemberTest() {
+        MemberEntity member = memberValidator.validateMember(3L);
+        assertThat(member.isDeleted()).isFalse();
+
         memberService.deleteMember(3L);
 
-        Assertions.assertThatThrownBy(() -> memberValidator.validateMember(3L)).isInstanceOf(DeletedMemberException.class);
+        assertThat(member.isDeleted()).isTrue();
+        assertThatThrownBy(() -> memberValidator.validateMember(3L)).isInstanceOf(DeletedMemberException.class);
     }
 }
