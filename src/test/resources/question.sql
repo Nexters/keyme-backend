f94e4f6e9daad9c521ead2f50a5fc7dae092a220
RokwonK <wonrok97@naver.com>
2023-10-19 19:54:39 +0900

    🎨 question test sql 수정

--- src/test/resources/question.sql
+++ src/test/resources/question.sql
@@ -7,20 +7,12 @@ VALUES
     (2, 'ACTIVE', 'james', '1234567', 'keyme.space/original', 'keyme.space.thumbnail', '2023-08-05T16:07:00', '2023-08-05T16:07:00', false),
     (3, 'ACTIVE', 'peter', 'A1B2C3D', 'keyme.space/original', 'keyme.space.thumbnail', '2023-08-05T16:07:00', '2023-08-05T16:07:00', false);
 
-/*
-    Member Oauth 정보 추가
-*/
-
 INSERT INTO member_oauth (oauth_id, oauth_type, member_id)
 VALUES
     (1, 'KAKAO', 1),
     (2, 'APPLE', 2),
     (3, 'KAKAO', 3);
 
-
-/*
-    Member Device 정보 추가
-*/
 INSERT INTO member_device (member_device_id, member_id, token)
 VALUES
     (1, 1, 'token1'),
@@ -31,74 +23,54 @@ VALUES
 /*
     Question 추가
 */
-INSERT INTO `question` (`question_id`, `title`, `keyword`, `category_name`, `is_onboarding`)
+INSERT INTO question_category (`question_category_id`, `name`, `color`, `icon_url`)
 VALUES
-    (1, '함께 시간을 보내는 것을 좋아하시나요?', '함께', '인간관계', true),
-    (2, '새로운 사람들과 친해지는 것을 좋아하시나요?', '친해지는', '사회적_활동', true),
-    (3, '여행을 즐기시겠군요?', '여행', '레저_및_여가', true),
-    (4, '계획적이고 체계적인 성향을 가지고 계시나요?', '계획적', '성격_및_특성', true),
-    (5, '감정을 잘 드러내시진 않을 것 같군요?', '감정', '감정_표현', true),
-    (6, '자신의 의견을 확고하게 말하시나요?', '의견', '의사_표현', true),
-    (7, '도전적인 상황에서 도전을 즐기실 것 같네요?', '도전', '성취_및_도전', true),
-    (8, '새로운 아이디어와 경험에 열려있으실 것 같아요?', '아이디어', '창의성', true),
-    (9, '예술과 창작적인 분야에 흥미를 느끼시겠군요?', '예술', '예술과_문화', true),
-    (10, '타인과의 협력을 중요하게 생각하시나요?', '협력', '인간관계', true);
-INSERT INTO `question` (`question_id`, `title`, `keyword`, `category_name`, `is_onboarding`)
-VALUES
-    (11, '새로운 사람들과 대화하는 것을 즐기시겠군요?', '대화', '사회적_활동', false),
-    (12, '규칙과 규제를 엄격하게 준수하시나요?', '규칙', '성격_및_특성', false),
-    (13, '문제가 발생했을 때 논리적으로 대처하실 것 같아요?', '대처', '문제_해결', false),
-    (14, '새로운 환경과 변화에 적응하는 것을 좋아하시겠군요?', '적응', '삶의_변화', false),
-    (15, '다른 사람들에게 조언을 주는 것을 좋아하시겠군요?', '조언', '인간관계', false),
-    (16, '혼자만의 시간을 즐기시겠군요?', '혼자', '개인적_성향', false);
+    (1, '돈관리', '568049', 'https://d2z2a95epq6bmg.cloudfront.net/icon/money.png'),
+    (2, '열정', 'F37952', 'https://d2z2a95epq6bmg.cloudfront.net/icon/passion.png'),
+    (3, '지능', 'D6EC63', 'https://d2z2a95epq6bmg.cloudfront.net/icon/intelligence.png'),
+    (4, '유머', '643FFF', 'https://d2z2a95epq6bmg.cloudfront.net/icon/humor.png'),
+    (5, '신체', 'EEAFB1', 'https://d2z2a95epq6bmg.cloudfront.net/icon/body.png'),
+    (6, '먹거리', 'A74850', 'https://d2z2a95epq6bmg.cloudfront.net/icon/food.png'),
+    (7, '센스', 'A9DBC3', 'https://d2z2a95epq6bmg.cloudfront.net/icon/sense.png'),
+    (8, '즉흥/계획', 'BF36FE', 'https://d2z2a95epq6bmg.cloudfront.net/icon/planning.png'),
+    (9, '감성/이성', '89B5F6', 'https://d2z2a95epq6bmg.cloudfront.net/icon/sensibility.png'),
+    (10, '관계', '905CFF', 'https://d2z2a95epq6bmg.cloudfront.net/icon/relationships.png'),
+    (11, '애정', 'FB5563', 'https://d2z2a95epq6bmg.cloudfront.net/icon/affection.png');
 
-/*
-    통계정보 추가
-*/
-INSERT INTO statistic (id, owner_id, question_id, solver_count, owner_score, solver_avg_score, match_rate, created_at, updated_at)
+INSERT INTO `question` (`question_id`, `title`, `keyword`, `question_category_id`, `category_name`, `is_onboarding`)
 VALUES
-    (1, 1, 1, 3, 5, 1, 20, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
-    (2, 1, 2, 3, 5, 2, 22, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
-    (3, 1, 3, 3, 5, 2, 25, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
-    (4, 1, 4, 3, 5, 3, 33.33, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
-    (5, 1, 5, 3, 5, 4, 50, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
-    (6, 1, 6, 3, 5, 5, 100, '2023-08-05T16:07:00', '2023-08-05T16:07:00');
-
-INSERT INTO statistic (id, owner_id, question_id, solver_count, owner_score, solver_avg_score, match_rate, created_at, updated_at)
-VALUES
-    (7, 2, 1, 0, 5, 5, 100, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
-    (8, 2, 2, 1, 5, 5, 0, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
-    (9, 2, 3, 1, 5, 5, 0, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
-    (10, 2, 4, 1, 5, 5, 0, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
-    (11, 2, 5, 1, 5, 5, 0, '2023-08-05T16:07:00', '2023-08-05T16:07:00');
---     (7, 1, 7, 3, 5, 1, 20, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
---     (8, 1, 8, 3, 5, 1, 20, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
-
+    (1, '님은 아이돌 덕질에 일가견이 있다.', '주간아이돌', 2, 'PASSION', true),
+    (2, '님은 관심 있는 사람에게 밥을 먼저 먹자고할 수 있다.', '플러팅 장인', 2, 'PASSION', true),
+    (3, '님은 하여자(하남자)다.', '하여자', 3, 'INTELLIGENCE', true),
+    (4, '님의 유튜브 알고리즘은 깨끗하다.', '순수한 뇌', 3, 'INTELLIGENCE', true),
+    (5, '님은 N번 까지 고백 공격을 해봤을것 같다.', '나무꾼', 4, 'HUMOR', true),
+    (6, '님은 번호를 따여봤을 것 같다.', '김태희', 5, 'BODY', true),
+    (7, '님은 나이보다 어려보인다.', '동안', 5, 'BODY', true),
+    (8, '님은 주변의 변화를 빠르게 알아차리는 편이다.', '눈썰미', 7, 'SENSE', true),
+    (9, '님은 너무 다정해서 유죄인간이다.', '유죄인간', 7, 'SENSE', true),
+    (10, '님은 친구 전화를 귀찮아서 안받은 적이 많다.', '귀차니즘', 8, 'PLANNING', true);
 
 
 /*
-    member 1이 테스트 추가 - test, question_bundle
+    Test 추가
 */
 INSERT INTO test (test_id, owner_id, is_onboarding, title, created_at, updated_at)
 VALUES
-    (1, 1, true, '함께 시간을 보내는 것을 좋아하시나요?', '2023-08-10T16:07:00', '2023-08-10T16:07:00'),
-    (2, 1, false, '새로운 사람들과 대화하는 것을 즐기시겠군요?', '2023-08-10T16:07:00', '2023-08-10T16:07:00');
+    (1, 1, true, '님은 아이돌 덕질에 일가견이 있다.', '2023-08-10T16:07:00', '2023-08-10T16:07:00'),
+    (2, 2, true, '님은 아이돌 덕질에 일가견이 있다.', '2023-08-10T16:07:00', '2023-08-10T16:07:00');
 INSERT INTO question_bundle (test_id, question_id)
 VALUES
     (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8), (1, 9), (1, 10),
-    (2, 11), (2, 12), (2, 13);
+    (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (2, 8), (2, 9), (2, 10);
 
 /*
-    위 테스트의 테스트결과들 추가 - test_result, question_solved
-        - 출제자의 result 추가 1
-        - 기명유저의 result 추가 1
-        - 익명유저의 result 추가 1
+    Test 결과 추가
 */
 INSERT INTO test_result (test_result_id, test_id, solver_id, match_rate, created_at, updated_at)
 VALUES
     (1, 1, 1, 100, '2023-08-10T17:07:00', '2023-08-10T17:07:00'),
-    (2, 1, null, 80, '2023-08-10T17:37:00', '2023-08-10T17:37:00'),
-    (3, 1, 2, 60, '2023-08-10T18:00:00', '2023-08-10T18:00:00');
+    (2, 1, null, 80, '2023-08-10T17:37:00', '2023-08-10T17:37:00');
+--     (3, 2, 2, 80, '2023-08-10T17:37:00', '2023-08-10T17:37:00');
 INSERT INTO question_solved(question_solved_id, test_result_id, question_id, question_owner_id, score, created_at, updated_at)
 VALUES
     (1, 1, 1, 1, 4, '2023-08-10T17:07:00', '2023-08-10T17:07:00'),
@@ -123,22 +95,48 @@ VALUES
     (18, 2, 8, 1, 4, '2023-08-11T17:07:00', '2023-08-11T17:07:00'),
     (19, 2, 9, 1, 1, '2023-08-11T17:07:00', '2023-08-11T17:07:00'),
     (20, 2, 10, 1, 2, '2023-08-11T17:07:00', '2023-08-11T17:07:00');
-INSERT INTO question_solved(question_solved_id, test_result_id, question_id, question_owner_id, score, created_at, updated_at)
-VALUES
-    (21, 3, 1, 1, 2, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
-    (22, 3, 2, 1, 1, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
-    (23, 3, 3, 1, 4, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
-    (24, 3, 4, 1, 3, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
-    (25, 3, 5, 1, 5, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
-    (26, 3, 6, 1, 5, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
-    (27, 3, 7, 1, 2, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
-    (28, 3, 8, 1, 4, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
-    (29, 3, 9, 1, 3, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
-    (30, 3, 10, 1, 5, '2023-08-11T17:17:00', '2023-08-11T17:17:00');
+-- INSERT INTO question_solved(question_solved_id, test_result_id, question_id, question_owner_id, score, created_at, updated_at)
+-- VALUES
+--     (21, 3, 1, 2, 2, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
+--     (22, 3, 2, 2, 1, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
+--     (23, 3, 3, 2, 4, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
+--     (24, 3, 4, 2, 3, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
+--     (25, 3, 5, 2, 5, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
+--     (26, 3, 6, 2, 5, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
+--     (27, 3, 7, 2, 2, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
+--     (28, 3, 8, 2, 4, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
+--     (29, 3, 9, 2, 3, '2023-08-11T17:17:00', '2023-08-11T17:17:00'),
+--     (30, 3, 10, 2, 5, '2023-08-11T17:17:00', '2023-08-11T17:17:00');
 
 /*
-    test result code 추가
+    Test Result Code 추가
 */
 INSERT INTO test_result_code(result_id, code)
 VALUES
-    (3, 'KW2EDq')
+    (2, 'KW2EDw'),
+    (3, 'KW2EDq');
+
+
+/*
+    Statistic 추가
+*/
+INSERT INTO statistic (id, owner_id, question_id, solver_count, owner_score, solver_avg_score, match_rate, created_at, updated_at)
+VALUES
+    (1, 1, 1, 3, 5, 1, 20, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
+    (2, 1, 2, 3, 5, 2, 22, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
+    (3, 1, 3, 3, 5, 2, 25, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
+    (4, 1, 4, 3, 5, 3, 33.33, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
+    (5, 1, 5, 3, 5, 4, 50, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
+    (6, 1, 6, 3, 5, 5, 100, '2023-08-05T16:07:00', '2023-08-05T16:07:00');
+
+INSERT INTO statistic (id, owner_id, question_id, solver_count, owner_score, solver_avg_score, match_rate, created_at, updated_at)
+VALUES
+    (7, 2, 1, 2, 5, 5, 100, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
+    (8, 2, 2, 1, 5, 5, 0, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
+    (9, 2, 3, 1, 5, 5, 0, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
+    (10, 2, 4, 1, 5, 5, 0, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
+    (11, 2, 5, 1, 5, 5, 0, '2023-08-05T16:07:00', '2023-08-05T16:07:00');
+--     (7, 1, 7, 3, 5, 1, 20, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
+--     (8, 1, 8, 3, 5, 1, 20, '2023-08-05T16:07:00', '2023-08-05T16:07:00'),
+
+
